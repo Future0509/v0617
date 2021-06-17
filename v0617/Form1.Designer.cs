c4d82aa0514da9ce3829bcc4f5b32f6cbc8f36d0
A601-20 <tnk.dat.class@gmail.com>
2021-06-17 12:22:02 +0900

    一つだけ動いた

--- v0617/Form1.Designer.cs
+++ v0617/Form1.Designer.cs
@@ -35,12 +35,13 @@ namespace v0617
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(173, 60);
+            this.label1.Location = new System.Drawing.Point(216, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 0;
@@ -57,9 +58,9 @@ namespace v0617
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(351, 195);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.Size = new System.Drawing.Size(41, 24);
             this.label3.TabIndex = 1;
-            this.label3.Text = "label3";
+            this.label3.Text = "（・∀・）\r\n\r\n";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
@@ -75,24 +76,36 @@ namespace v0617
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(693, 361);
+            this.label4.Location = new System.Drawing.Point(471, 275);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.Size = new System.Drawing.Size(41, 12);
             this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.label4.Text = "（・∀・）";
             this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(436, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "（・∀・）";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(907, 642);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +118,7 @@ namespace v0617
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- v0617/Form1.cs
+++ v0617/Form1.cs
@@ -12,16 +12,25 @@ namespace v0617
 {
     public partial class Form1 : Form
     {
-        int vx = -10;
-        int vy = -10;
-
+        int vx = rand.Next(-10, 11);
+        int vy = rand.Next (-10,11);
+        
+        private static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(ClientSize.Height - label1.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            Point spos = MousePosition;
+            Point fpos = PointToClient(spos);
+            label3.Left = fpos.X - label3.Width / 2;
+            label3.Top = fpos.Y - label3.Height / 2;
+            label3.Text = $"{fpos.X}, {fpos.Y}";
+
             label1.Left += vx;
             label1.Top += vy;
             if (label1.Left < 0)
@@ -40,16 +49,41 @@ namespace v0617
             {
                 vy = -Math.Abs(vy);
             }
-            Point spos = MousePosition;
-            Point fpos = PointToClient(spos);
-            label3.Left = fpos.X - label3.Width / 2;
-            label3.Top = fpos.Y - label3.Height / 2;
-            label3.Text = $"{fpos.X}, {fpos.Y}";
+            label4.Left += vx;
+            label4.Top += vy;
+            if (label4.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label4.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label4.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if (label4.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+
+            if ((fpos.X >= label1.Left)
+               && (fpos.X < label1.Right)
+               && (fpos.Y >= label1.Top)
+               && (fpos.Y < label1.Bottom)
+               )
+            {
+
+            }
         }
         private void label1_Click(object sender, EventArgs e)
-        {
-            timer1.Enabled = false;
-        }
+        
+            
+            {
+                timer1.Enabled = false;
+            }
+        
         private void label2_Click(object sender, EventArgs e)
         {
 
@@ -61,6 +95,15 @@ namespace v0617
         }
 
         private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
